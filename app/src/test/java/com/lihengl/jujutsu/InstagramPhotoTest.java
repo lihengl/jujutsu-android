4fbed99c9ac88ad403b27b9b9a3e000e0a162b03
LIHENG <admin@lihengl.com>
2015-07-29 11:19:38 +0800

    Fix lint/layout errors and setup unit test

---
+++ app/src/test/java/com/lihengl/jujutsu/InstagramPhotoTest.java
@@ -0,0 +1,12 @@
+package com.lihengl.jujutsu;
+
+import junit.framework.TestCase;
+
+
+public class InstagramPhotoTest extends TestCase {
+
+    public void testHelloWorld() {
+        assertTrue(true);
+    }
+
+}
