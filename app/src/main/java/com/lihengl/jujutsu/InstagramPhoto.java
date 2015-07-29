3600af625d67349151afcd0f9f656707cd4d5547
Li-Heng Liang <admin@lihengl.com>
2015-07-29 08:36:44 +0800

    Implement along with the walkthrough video

---
+++ app/src/main/java/com/lihengl/jujutsu/InstagramPhoto.java
@@ -0,0 +1,9 @@
+package com.lihengl.jujutsu;
+
+public class InstagramPhoto {
+	public String imageUrl;
+	public String username;
+	public String caption;
+	public int imageHeight;
+	public int likesCount;
+}
--- app/src/main/java/com/lihengl/jujutsu/MainActivity.java
+++ app/src/main/java/com/lihengl/jujutsu/MainActivity.java
@@ -1,17 +1,75 @@
 package com.lihengl.jujutsu;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    public static final String CLIENT_ID = "a08d215934244b2dabe76962220287c9";
+    private ArrayList<InstagramPhoto> photos;
+    private InstagramPhotosAdapter aPhotos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        photos = new ArrayList<>();
+        aPhotos = new InstagramPhotosAdapter(this, photos);
+        ListView lvPhotos = (ListView) findViewById(R.id.lvPhotos);
+        lvPhotos.setAdapter(aPhotos);
+        fetchPopularPhotos();
+    }
+
+    public void fetchPopularPhotos() {
+        AsyncHttpClient client = new AsyncHttpClient();
+        String popularApi = "https://api.instagram.com/v1/media/popular?client_id=" + CLIENT_ID;
+
+        client.get(popularApi, null, new JsonHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                JSONArray photosJSON = null;
+
+                try {
+                    photosJSON = response.getJSONArray("data");
+                    for (int i = 0; i < photosJSON.length(); i++) {
+                        JSONObject photoJSON = photosJSON.getJSONObject(i);
+                        InstagramPhoto photo = new InstagramPhoto();
+                        photo.imageHeight = photoJSON.getJSONObject("images").getJSONObject("standard_resolution").getInt("height");
+                        photo.imageUrl    = photoJSON.getJSONObject("images").getJSONObject("standard_resolution").getString("url");
+                        photo.username    = photoJSON.getJSONObject("user").getString("username");
+                        photo.caption     = photoJSON.getJSONObject("caption").getString("text");
+                        photo.likesCount  = photoJSON.getJSONObject("likes").getInt("count");
+                        photos.add(photo);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                aPhotos.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                Log.e("NETWORK", responseString);
+            }
+
+        });
     }
 
     @Override
